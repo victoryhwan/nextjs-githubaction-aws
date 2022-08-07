140c03632dbb3522b5d1e55f6273275a4af8b90b
kihwan lee <victoryhwan7@gmail.com>
2022-08-08 02:17:29 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Learn about Next.js in an interactive course with quizzes!!</p>
           </a>
 
           <a
